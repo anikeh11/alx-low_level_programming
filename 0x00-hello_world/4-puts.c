e32d8be67cbf209fb4d6a81eb4753fbba7076e75
anikeh11ghghttps://github.com/anikeh11/alx-system_engineering-devops.git <hikumapayi@gmail.com>
2022-03-17 16:25:57 -0700

    C hello_world task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- *  * main - function
- *   *Description: used to print out a string
- *    * Return: Always 0 (Success)
- *     */
+ * main - function
+ *
+ * Description: used to print out a string
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
