e2a9d85051f83c360edc8f9196046cb1938a3b7a
anikeh11ghghttps://github.com/anikeh11/alx-system_engineering-devops.git <hikumapayi@gmail.com>
2022-03-17 18:03:11 -0700

    C if,else,while task 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
-
-
-
-
-
-
+/**
+ * main - prints the alphabet in lowercase, folloewd by a new line
+ *
+ * Return: (0) Success
+ */
 int main(void)
 {
 
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
-
-            
-                                                                                    	
-
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ *
+ * Return : (0)
+ */
 int main(void)
 {
 	int i;
